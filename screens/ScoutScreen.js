8ff2da82cce09ef28e8a2119a046f87eb8e5fcd6
Emily <emily@sturman.org>
2019-03-04 15:11:48 -0800

    Completed basic UI (Home and scouting screen)

--- screens/ScoutScreen.js
+++ screens/ScoutScreen.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import {
   Dimensions,
-  FlatList,
   StyleSheet,
   Text,
   View,
+  TextInput,
+  Platform,
 } from "react-native";
+import ReactNative from 'react-native';
+import { KeyboardAwareFlatList } from 'react-native-keyboard-aware-scroll-view';
 import Touchable from "react-native-platform-touchable";
 import CardNonTouchable from "../other/CardNonTouchable.js";
 import {SafeAreaView} from "react-navigation";
-import CheckBox from 'react-native-elements';
+import { CheckBox } from 'react-native-elements';
 
 const ACCENT_COLOR = '#03b0ff';
 const ACCENT_COLOR_DARK = '#0374b2';
@@ -20,7 +23,7 @@ const PLUS_ONE_COLOR_DARK= '#4ca84a';
 
 const PlusMinusMenu = ({ title, num, isHatch, handler }) => (
   <View style={{marginBottom: 7}}>
-    <Text style={{marginBottom: 5}}>
+    <Text style={{marginBottom: 5, marginHorizontal: 5}}>
       <Text style={styles.cardTitle}>{title}</Text>
       <Text style={styles.cardTextSecondary}>   {num}</Text>
     </Text>
@@ -41,6 +44,90 @@ const PlusMinusMenu = ({ title, num, isHatch, handler }) => (
   </View>
 );
 
+const OptionsMenu = ({ options, handler }) => options.map((option, index) => (
+  <CheckBox
+    key={index}
+    title={option.title}
+    checkedIcon='dot-circle-o'
+    uncheckedIcon='circle-o'
+    checked={option.checked}
+    containerStyle={styles.checkBoxContainer}
+    onPress={() => handler(option.goal)}
+  />
+));
+
+const Sandstorm = ({ hatch, cargo, handler, onSecondLevel, startingPosHandler, crossedHabLine, crossHabHandler }) => (
+  <View>
+    <View>
+      <Text style={styles.cardTitle}>Starting Position</Text>
+      <OptionsMenu options={[
+        {title: 'Level 1', checked: !onSecondLevel, goal: false},
+        {title: 'Level 2', checked: onSecondLevel, goal: true}
+      ]} handler={startingPosHandler} />
+    </View>
+    <CheckBox title='Crossed hab line:'
+              checked={crossedHabLine}
+              containerStyle={[styles.checkBoxContainer, {marginLeft: 0, paddingLeft: 0}]}
+              textStyle={[styles.cardTitle, {marginLeft: 0, paddingLeft: 0}]}
+              iconRight
+              onPress={() => crossHabHandler(crossedHabLine)}
+    />
+    <PlusMinusMenu title="Hatch" num={hatch} isHatch={true} handler={handler} />
+    <PlusMinusMenu title="Cargo" num={cargo} isHatch={false} handler={handler} />
+  </View>
+);
+
+const Teleop = ({ hatch, cargo, handler }) => (
+  <View>
+    <PlusMinusMenu title="Hatch" num={hatch} isHatch={true} handler={handler} />
+    <PlusMinusMenu title="Cargo" num={cargo} isHatch={false} handler={handler} />
+  </View>
+);
+
+const EndGame = ({ level, climbHandler, buddyClimb, buddyClimbHandler  }) => (
+  <View>
+    <View>
+      <Text style={styles.cardTitle}>Hab Climb</Text>
+      <OptionsMenu options={[
+        {title: 'None', checked: level === 0, goal: 0},
+        {title: 'Level 1', checked: level === 1, goal: 1},
+        {title: 'Level 2', checked: level === 2, goal: 2},
+        {title: 'Level 3', checked: level === 3, goal: 3},
+      ]} handler={climbHandler} />
+    </View>
+    <View>
+      <Text style={styles.cardTitle}>Buddy Climb (Successful)</Text>
+      <OptionsMenu options={[
+        {title: 'None', checked: buddyClimb === 0, goal: 0},
+        {title: '1 robot', checked: buddyClimb === 1, goal: 1},
+        {title: '2 robots', checked: buddyClimb === 2, goal: 2},
+      ]} handler={buddyClimbHandler} />
+    </View>
+  </View>
+);
+
+const PostMatch = ({ brokeDown, brokeDownHandler, changeText, scroll }) => (
+  <View>
+    <CheckBox title='Robot broke down:'
+              checked={brokeDown}
+              containerStyle={[styles.checkBoxContainer, {marginLeft: 0, paddingLeft: 0}]}
+              textStyle={[styles.cardTitle, {marginLeft: 0, paddingLeft: 0}]}
+              iconRight
+              onPress={() => brokeDownHandler(brokeDown)}
+    />
+    <View>
+      <Text style={styles.cardTitle}>Comments:</Text>
+      <TextInput multiline={true}
+                 style={styles.textInput}
+                 placeholder='Enter comments here'
+                 onEndEditing={(text) => changeText(text.nativeEvent.text)}
+                 onFocus={(event) => scroll(ReactNative.findNodeHandle(event.target))}
+                 editable={true}
+      />
+    </View>
+  </View>
+);
+
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
     header: null
@@ -52,13 +139,31 @@ export default class HomeScreen extends React.Component {
       sandstorm: {
         hatch: 0,
         cargo: 0,
+        onSecondLevel: false,
+        crossedHabLine: false,
       },
       teleop: {
         hatch: 0,
         cargo: 0,
+      },
+      endgame: {
+        climb: 0,
+        buddyClimb: 0,
+      },
+      postgame: {
+        brokeDown: false,
+        comments: '',
       }
     };
     this.sandstormHandler = this.sandstormHandler.bind(this);
+    this.teleopHandler = this.teleopHandler.bind(this);
+    this.startingPosHandler = this.startingPosHandler.bind(this);
+    this.crossedHab = this.crossedHab.bind(this);
+    this.climbHandler = this.climbHandler.bind(this);
+    this.buddyClimbHandler = this.buddyClimbHandler.bind(this);
+    this.brokeDownHandler = this.brokeDownHandler.bind(this);
+    this.changeText = this.changeText.bind(this);
+    this.scrollToInput = this.scrollToInput.bind(this);
   }
 
   sandstormHandler(num, plus, hatch) {
@@ -67,39 +172,82 @@ export default class HomeScreen extends React.Component {
     else if (stateNum > 0) stateNum -= 1;
     else stateNum = 0;
 
-    console.log(this.state.sandstorm.cargo);
-
     if (hatch) {
-      this.setState(prevState => ({
+      this.setState({
         sandstorm: {
+          ...this.state.sandstorm,
           hatch: stateNum,
-          cargo: prevState.sandstorm.cargo
         }
-      }))
+      })
     } else {
-      this.setState(prevState => ({
+      this.setState({
         sandstorm: {
-          hatch: prevState.sandstorm.hatch,
+          ...this.state.sandstorm,
           cargo: stateNum
         }
-      }))
+      })
     }
   }
 
-  Sandstorm = ({ hatch, cargo, handler }) => (
-    <View>
-      <CheckBox
-        title='Click Here'
-        checked={true}
-      />
-      <PlusMinusMenu title="Hatch" num={hatch} isHatch={true} handler={handler} />
-      <PlusMinusMenu title="Cargo" num={cargo} isHatch={false} handler={handler} />
-    </View>
-  );
+  teleopHandler(num, plus, hatch) {
+    let stateNum = num;
+    if (plus) stateNum += 1;
+    else if (stateNum > 0) stateNum -= 1;
+    else stateNum = 0;
+
+    if (hatch) {
+      this.setState({
+        teleop: {
+          ...this.state.teleop,
+          hatch: stateNum,
+        }
+      })
+    } else {
+      this.setState({
+        teleop: {
+          ...this.state.teleop,
+          cargo: stateNum
+        }
+      })
+    }
+  }
+
+  startingPosHandler(goal) {
+    this.setState({sandstorm: {...this.state.sandstorm, onSecondLevel: goal}});
+  }
+
+  crossedHab(value) {
+    this.setState({sandstorm: {...this.state.sandstorm, crossedHabLine: !value}})
+  }
+
+  climbHandler(goal) {
+    console.log(goal);
+    this.setState({endgame: {...this.state.endgame, climb: goal}});
+  }
+
+  buddyClimbHandler(goal) {
+    this.setState({endgame: {...this.state.endgame, buddyClimb: goal}});
+  }
+
+  brokeDownHandler(value) {
+    this.setState({postgame: {...this.state.postgame, brokeDown: !value}});
+  }
+
+  changeText(text) {
+    console.log(text);
+    this.setState({postgame: {...this.state.postgame, comments: text}});
+  }
+
+  scrollToInput (reactNode) {
+    this.scroll.props.scrollToFocusedInput(reactNode)
+  }
 
   render() {
     const screenWidth = Dimensions.get('window').width;
     const {navigate} = this.props.navigation;
+    let keyboardDismissMode;
+    if (Platform.OS === 'ios') keyboardDismissMode = 'interactive';
+    else keyboardDismissMode = 'on-drag';
     return (
       <SafeAreaView style={{flex: 1}} forceInset={{bottom: 'never'}}>
         <View
@@ -118,14 +266,37 @@ export default class HomeScreen extends React.Component {
             <Text style={styles.submitButtonText}>Submit</Text>
           </Touchable>
         </View>
-        <FlatList
+        <KeyboardAwareFlatList
+          innerRef={ref => {this.scroll = ref}}
+          enableOnAndroid={true}
+          enableResetScrollToCoords={false}
+          extraHeight={150}
           style={styles.container}
+          keyboardDismissMode={keyboardDismissMode}
           data={[
-            {key: 'Sandstorm', content: (<this.Sandstorm hatch={this.state.sandstorm.hatch} cargo={this.state.sandstorm.cargo} handler={this.sandstormHandler} />)},
-            {key: 'Statistics', content: this.Statistics},
-            {key: 'Recent Matches', content: this.RecentMatches},
-            {key: 'Rankings', content: this.Rankings},
-            {key: '', content: (<View></View>), style: {opacity: 0}}
+            {key: 'Sandstorm', content: (<Sandstorm hatch={this.state.sandstorm.hatch}
+                                                    cargo={this.state.sandstorm.cargo}
+                                                    handler={this.sandstormHandler}
+                                                    onSecondLevel={this.state.sandstorm.onSecondLevel}
+                                                    crossedHabLine={this.state.sandstorm.crossedHabLine}
+                                                    startingPosHandler={this.startingPosHandler}
+                                                    crossHabHandler={this.crossedHab} />)
+            },
+            {key: 'Teleop', content: (<Teleop hatch={this.state.teleop.hatch}
+                                              cargo={this.state.teleop.cargo}
+                                              handler={this.teleopHandler} />)
+            },
+            {key: 'End Game', content: (<EndGame level={this.state.endgame.climb}
+                                                 climbHandler={this.climbHandler}
+                                                 buddyClimb={this.state.endgame.buddyClimb}
+                                                 buddyClimbHandler={this.buddyClimbHandler} />)
+            },
+            {key: 'Post Match', content: (<PostMatch brokeDown={this.state.postgame.brokeDown}
+                                                     brokeDownHandler={this.brokeDownHandler}
+                                                     scroll={this.scrollToInput}
+                                                     changeText={this.changeText} />)
+            },
+            {key: '', content: (<View />), style: {opacity: 0}}
           ]}
           renderItem={({item}) => <CardNonTouchable title={item.key} content={item.content} style={item.style} handler={item.handler}/>}
         />
@@ -185,6 +356,8 @@ const styles = StyleSheet.create({
   cardTitle: {
     fontFamily: 'open-sans-bold',
     fontSize: 16,
+    marginLeft: 5,
+    color: '#000',
   },
   cardText: {
     fontFamily: 'open-sans-bold',
@@ -202,7 +375,7 @@ const styles = StyleSheet.create({
     backgroundColor: MINUS_ONE_COLOR,
     alignItems: 'center',
     padding: 10,
-    marginRight: 5,
+    marginHorizontal: 5,
     borderRadius: 5
   },
   plusOne: {
@@ -210,7 +383,22 @@ const styles = StyleSheet.create({
     backgroundColor: PLUS_ONE_COLOR,
     alignItems: 'center',
     padding: 10,
-    marginLeft: 5,
+    marginHorizontal: 5,
     borderRadius: 5,
   },
+  checkBoxContainer: {
+    backgroundColor: 'transparent',
+    borderColor: 'transparent',
+    margin: 0,
+    padding: 5,
+  },
+  textInput: {
+    minHeight: 75,
+    backgroundColor: '#f0f0f0',
+    padding: 10,
+    borderRadius: 5,
+    fontFamily: 'open-sans',
+    fontSize: 14,
+    margin: 5,
+  }
 });
